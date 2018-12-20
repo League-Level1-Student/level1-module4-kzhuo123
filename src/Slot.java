568fdf696affd3faab7c86b4d34e8acdd464ed80
kzhuo123 <league@WorthyMini-5.attlocal.net>
2018-12-19 19:53:43 -0800

    u89piop
    uiopuiopuiopuiopuiop

--- src/Slot.java
+++ src/Slot.java
@@ -1,12 +1,16 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Random;
 
+import javax.imageio.ImageIO;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Slot implements ActionListener {
@@ -16,7 +20,11 @@ JLabel label = new JLabel();
 JLabel label1;
 JLabel label2;
 JButton button=new JButton();
-	
+ImageIcon coin;
+ImageIcon cherry;
+ImageIcon seven;
+
+
 	
 public static void main(String[] args) {
 	Slot run = new Slot();
@@ -25,6 +33,14 @@ public static void main(String[] args) {
 	
 
 public void drawslot() {
+	try {
+		coin=new ImageIcon(ImageIO.read(this.getClass().getResource("coin.jpg")));
+		cherry=new ImageIcon(ImageIO.read(this.getClass().getResource("cherry.jpg")));
+		seven=new ImageIcon(ImageIO.read(this.getClass().getResource("seven.jpg")));
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	frame=new JFrame();
 	panel=new JPanel();
 	frame.setVisible(true);
@@ -51,9 +67,44 @@ private JLabel showImage(String fileName) {
 
 @Override
 public void actionPerformed(ActionEvent e) {
+	Random r = new Random();
+	Random r1= new Random();
+	Random r2= new Random();
 	JButton buttonClicked = (JButton) e.getSource();
 	if (buttonClicked==button) {
-		
+		int n = r.nextInt(3);
+		int n1= r1.nextInt(3);
+		int n2= r2.nextInt(3);
+		if (n==0) {
+			label.setIcon(coin);
+		}
+		if (n==1) {
+			label.setIcon(cherry);
+		}
+		if (n==2) {
+			label.setIcon(seven);
+		}
+		if (n1==0) {
+			label1.setIcon(coin);
+		}
+		if (n1==1) {
+			label1.setIcon(cherry);
+		}
+		if (n1==2) {
+			label1.setIcon(seven);
+		}
+		if (n2==0) {
+			label2.setIcon(coin);
+		}
+		if (n2==1) {
+			label2.setIcon(cherry);
+		}
+		if (n2==2) {
+			label2.setIcon(seven);
+		}
+		if (n==n1&&n==n2) {
+			JOptionPane.showMessageDialog(null, "YOU WIN!");
+		}
 	}
 	
 }
